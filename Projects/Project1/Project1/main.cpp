7323839f721a686b1bb1c64ca71cf410698974d2
xevanx <59667380+xevanx@users.noreply.github.com>
2021-07-20 15:38:12 -0700

    first project

--- Projects/Project1/Project1/main.cpp
+++ Projects/Project1/Project1/main.cpp
@@ -9,7 +9,6 @@
 //System Libraries
 #include <iostream>
 #include <iomanip>
-#include <ctime>
 #include <string>
 #include <cstdlib>
 using namespace std;
@@ -21,144 +20,188 @@ using namespace std;
 
 //Function Prototypes
 struct GameBoard {
-    char spot; //A structure to store the entire game board
-    bool ship; //A check if the spot has a ship there
-};
+    string name;//Name of the ship in that spot
+    char spot;  //A structure to store the entire game board with x & y coordinates
+    bool ship;  //A bool for if a boat is in the spot or not
+}field[9][10];
 
-void prntBoard(GameBoard field[9][10]);
-void spawnShip(GameBoard field[9][10]);
-void checkHit(GameBoard*, char, char);
+void prntBoard();
+void setBoard();
+void spawnShip();
+void checkHit(char, char);
+bool checkEnd(bool);
 
 //Execution Begins Here
 int main(int argc, char** argv) {
-    //Declare Variable Data Types and Constants
-    GameBoard field[9][10];//Entire game board
-    char fireRow, fireColumn;//Input for where to fire on the board
+    char newStart;      // Input for if the game should restart or not
+    char fireX, fireY;    // Input for where to fire on the board
+    bool gameEnd = false; // A check to see if the game end conditions have been met
+    bool newGame = false; // A check to see if the player wants to play the game again
     
-    //Initialize Variables
-//    for (int i = 0; i <= 9; i++) {
-//        for (int j = 0; j <= 9; j++){
-//            field[i][j].ship = false;
-//            cout << field[i][j].ship << endl;
-//        }
-//    }
+    //Initializing the board with the first printing
+    do {
+        setBoard();
+        prntBoard();
+        spawnShip();
+
+        do {
+            prntBoard();
+            cout << "Please select a space to fire at, column first then row" << endl;
+            cin >> fireX >> fireY;
+            checkHit(fireX, fireY);
+            cout << endl;
+            checkEnd(gameEnd);
+        } while (gameEnd = true);
+
+        cout << "Congrats for sinking all the ships"<< endl;
+        cout << "Would you like to play again?" << endl;
+        cout << "Y or N" << endl;
+        cin >> newStart;
+        if (newStart = 'Y') {
+            newGame = true;
+        }
+        else {
+            return 0;
+        }
+    } while (newGame = true);
     
-    //Process or map Inputs to Outputs
-    spawnShip(field);
-    
-    
-    //Display Outputs
-    prntBoard(field);
-    
-    cout << "Please select a space to fire at, column first then row" << endl;
-    cin >> fireColumn >> fireRow;
-    
-    if (fireColumn == 'A' || fireColumn == 'a') {
-        fireColumn = '0';
-    }
-    else if (fireColumn == 'B' || fireColumn == 'b') {
-        fireColumn = '1';
-    }
-    else if (fireColumn == 'C' || fireColumn == 'c') {
-        fireColumn = '2';
-    }
-    else if (fireColumn == 'D' || fireColumn == 'd') {
-        fireColumn = '3';
-    }
-    else if (fireColumn == 'E' || fireColumn == 'e') {
-        fireColumn = '4';
-    }
-    else if (fireColumn == 'F' || fireColumn == 'f') {
-        fireColumn = '5';
-    }
-    else if (fireColumn == 'G' || fireColumn == 'g') {
-        fireColumn = '6';
-    }
-    else if (fireColumn == 'H' || fireColumn == 'h') {
-        fireColumn = '7';
-    }
-    else if (fireColumn == 'I' || fireColumn == 'i') {
-        fireColumn = '8';
-    }
-    else if (fireColumn == 'J' || fireColumn == 'j') {
-        fireColumn = '9';
-    }
-    
-    checkHit(&field[fireColumn][fireRow], fireRow, fireColumn);
-    
-    prntBoard(field);
-    //Exit stage right!
     return 0;
 }
 
-void spawnShip (GameBoard field[9][10]) {
-    int shipX;
-    int shipY;
-    int vertOr;//Random Number to decide if the ship is placed vertical or horizontal
-    srand(time(0));
-    
-    vertOr = (rand() % 2) + 1;//1 means vertical and 2 means horizontal placement
-    shipX = (rand() % 10) + 1;
-    shipY = (rand() % 10) + 1;
-    
-    cout << shipX << " " << shipY << endl;
-    
-    field[shipX][shipY].ship = true;
-    
-    if (vertOr == 1) {
-        cout << vertOr << " means vertical" << endl;
-        for (int i = 0; i < 4; i++) {
-            shipY = shipY + 1;
-            cout << shipY << endl;
-            field[shipX][shipY].ship = false;
-            cout << "Done" << endl;
-            if (field[shipX][shipY].ship = false) {
-                cout << "Working" << endl;
-            }
-        }
-    }
-    else if (vertOr == 2) {
-        cout << vertOr << " means horizontal" << endl;
-        for (int i = 0; i < 4; i++) {
-            shipX = shipX + 1;
-            cout << shipX << endl;
-            field[shipX][shipY].ship = false;
-            cout << "Done" << endl;
-            if (field[shipX][shipY].ship = false) {
-                cout << "Working" << endl;
-            }
+void setBoard() {
+    char box = 254;
+    for (int x = 0; x <= 9; x++) {
+        for (int y = 0; y <= 10; y++) {
+            field[x][y].spot = box;
+            field[x][y].ship = false;
         }
     }
 }
 
-void prntBoard (GameBoard field[9][10]) {
+void spawnShip() {
+    string names[5] = {"Battleship", "Submarine", "Cruiser", "Frigate", "Escort"};
+    char orient;
+    char shipX;
+    char shipY;
+    
+    for (int i = 0; i <= 4; i++) {
+        field[shipX][shipY].spot = 'S';
+        if (i = 0) {
+            cout << "Choose where to place the first spot of the battleship" << endl;
+            cout << "First the X coordinate and then the Y" << endl;
+            cout << "Input the X coordinate as a number from 1-10" << endl;
+            cin >> shipX >> shipY;
+            cout << "Now choose if the battleship is vertical: v or horizontal: h" << endl;
+            cin >> orient;
+            for (int j = 1; j <= 5; j++) {
+                if(orient = 'v') {
+                    cout << "Please choose the next 4 spots above or below" << endl;
+                    cin >> shipY;
+                }
+                else if (orient = 'h') {
+                    cout << "Please choose the next 4 spots to the left or right" << endl;
+                    cin >> shipX;
+                }
+                field[shipX][shipY].ship = true;
+            }    
+        }
+        else if (i = 1) {
+            cout << "Choose where to place the first spot of the Cruiser" << endl;
+            cout << "First the X coordinate and then the Y" << endl;
+            cin >> shipX >> shipY;
+            cout << "Now choose if the " << names[i] << " is vertical: v or horizontal: h" << endl;
+            cin >> orient;
+            for (int j = 1; j <= 3; j++) {
+                if(orient = 'v') {
+                    cout << "Please choose the next 2 spots above or below" << endl;
+                    cin >> shipY;
+                }
+                else if (orient = 'h') {
+                    cout << "Please choose the next 2 spots to the left or right" << endl;
+                    cin >> shipX;
+                }
+                field[shipX][shipY].ship = true;
+            }    
+        }
+        else if (i = 2) {
+            cout << "Choose where to place the first spot of the frigate" << endl;
+            cout << "First the X coordinate and then the Y" << endl;
+            cin >> shipX >> shipY;
+            cout << "Now choose if the " << names[i] << " is vertical: v or horizontal: h" << endl;
+            cin >> orient;
+            for (int j = 1; j <= 4; j++) {
+                if(orient = 'v') {
+                    cout << "Please choose the next spot above or below" << endl;
+                    cin >> shipY;
+                }
+                else if (orient = 'h') {
+                    cout << "Please choose the next spots to the left or right" << endl;
+                    cin >> shipX;
+                }
+                field[shipX][shipY].ship = true;  
+            }
+        }
+        else if (i = 3) {
+            for (int j = 1; j <= 3; j++) {
+                if(orient = 'v') {
+                    cout << "Please choose the next 2 spots above or below" << endl;
+                    cin >> shipY;
+                }
+                else if (orient = 'h') {
+                    cout << "Please choose the next 2 spots to the left or right" << endl;
+                    cin >> shipX;
+                }
+                field[shipX][shipY].ship = true;
+            }    
+        }
+        else if (i = 4) {
+            for (int j = 1; j <= 4; j++) {
+                if(orient = 'v') {
+                    cout << "Please choose the next 3 spots above or below" << endl;
+                    cin >> shipY;
+                }
+                else if (orient = 'h') {
+                    cout << "Please choose the next 3 spots to the left or right" << endl;
+                    cin >> shipX;
+                }
+                field[shipX][shipY].ship = true;
+            }    
+        }
+    }
+}
+
+void prntBoard() {
     cout << endl;
     cout << "A  B  C  D  E  F  G  H  I  J" << endl;
-    for (int i = 0; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++){
-            if (field[i][j].ship = false) {
-                field[i][j].spot = 'X';
-                cout << field[i][j].spot << "  ";
-            }
-            else {
-                field[i][j].spot = 'O';
-                cout << field[i][j].spot << "  ";
-            }
-            if (j == 9) {
-                cout << i + 1 << endl;
-                break;
+    for (int x = 0; x <= 9; x++) {
+        for (int y = 0; y < 10; y++){
+            cout << field[x][y].spot << "  ";
+            if (y == 9) {
+                cout << x + 1 << endl;
             }
         }
     }
 }
 
-void checkHit (GameBoard *Board, char fireRow, char fireColumn) {
-    if ((*Board).ship = true) {
-        (*Board).spot == 'O';
-        cout << "HIT!!!" << endl;
+void checkHit (char fireX, char fireY) {
+    if (field[fireX][fireY].ship = true) {
+        field[fireX][fireY].ship = false;
+        field[fireX][fireY].spot = 'X';
+        cout << "That's a hit!!" << endl;
     }
-    else {
-        (*Board).spot == 'M';
-        cout << "MISS!!!" << endl;
+    else if (field[fireX][fireY].ship = false) {
+        field[fireX][fireY].spot = 'M';
+        cout << "You missed!" << endl;
     }
+}
+
+bool checkEnd (bool gameEnd) {
+    for (int x = 0; x <= 9; x++) {
+        for (int y = 0; y < 10; y++){
+            if (field[x][y].ship = true) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
